03c293eb3db3deaa80c4607f3fcef009d7c02ccc
Mohammed <v7mv123@gmail.com>
2026-02-08 23:29:37 +0300

    added (reference, temperature, top_p) functions

--- autoRAG_project/config/pipeline/views.py
+++ autoRAG_project/config/pipeline/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 import json
 from django.http import JsonResponse
-# Create your views here.
-import json
-from django.http import JsonResponse
-import json
-from django.http import JsonResponse
-
-import json
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -17,9 +9,9 @@ def questionnaire(request):
     if request.method == 'POST':
         # Read data from form POST
         
-        reference = request.POST.get('reference')
-        temperature = float(request.POST.get('temperature'))
-        top_p = float(request.POST.get('top_p'))
+        reference = reference(request.POST.get('reference'))
+        temperature = temperature(request.POST.get('creative'))
+        top_p = top_p(request.POST.get('strict'))
         
         
         # later --> workspace.insert(reference, temperature, top_p, embedding)
@@ -36,5 +28,30 @@ example of json(API) sent from frontend to backend (including the 9 answers):
     'top_p': 0.9
 }
 """
+def temperature(temperature_value):
+    temp = 0
+    if temperature_value == 'precise':
+        temp = 0.2
+    elif temperature_value == 'balanced':
+        0.5
+    else: # creative
+        temp = 0.8
+    return temp      
 
-            
+def top_p(top_p_value):  
+    top = 0
+    if top_p_value == 'strict':
+        top = 0.2
+    elif top_p_value == 'balanced':
+        top = 0.5
+    else: # explaratory
+        top = 0.9
+    return top
+        
+
+def reference(reference_value):
+    if reference_value == 'yes':
+        return True
+    else:
+        return False
+    
