e4f2f7094a807102cb091d9dbbbc35fb559c1d78
Rashedmu <kingrashed2004@gmail.com>
2026-02-20 02:07:49 +0300

    Added qdrant necessary settings to work

--- autoRAG_project/config/config/urls.py
+++ autoRAG_project/config/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pipeline.urls')),
+    path('api/documents/', include('documents.urls')),
 ]
