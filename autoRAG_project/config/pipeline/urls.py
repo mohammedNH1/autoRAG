9c6915601c4b71c3ea0c7460fe97e8c5e0c7753d
444101285 <444101285@student.ksu.edu.sa>
2026-03-04 00:52:09 +0300

    Enhance Chat Interface 2.0

--- autoRAG_project/config/pipeline/urls.py
+++ autoRAG_project/config/pipeline/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('submit-answers/', views.questionnaire, name='submit_answers'),
     path('workspace/<int:workspace_id>/', views.initiate_pipeline, name='initiate_pipeline'),
-
+    path('workspace/<int:workspace_id>/query/', views.query_handling, name='query_handling'),
 ]
